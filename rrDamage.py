fda315b1d2a26e91a0ce1e2b1b0ea47354ced2a6
Patrick <Patrickwiechert94@gmail.com>
2018-10-27 02:07:26 +0200

    msu

--- rrDamage.py
+++ rrDamage.py
@@ -82,14 +82,13 @@ async def getPlayerDamage(url,session,profildict,partylist):
     print("RawDamage URL: ", url)
     print("In RawDamage. damagelist: ", playerpartys)
 
-    url0 = url.replace("#war/details", "war/damage") + "/1"
+    url1 = url.replace("#war/details", "war/damage") + "/1"
 
     player = []
     damage = []
     counter = 1
 
-    url = url.replace("#war/details", "war/damage/0")
-    html = await fetch(session, url)
+    html = await fetch(session, url1)
     soup = await soup_d(html)
 
     for profil in soup.find_all(attrs={"class": "list_name pointer"}):
@@ -106,6 +105,7 @@ async def getPlayerDamage(url,session,profildict,partylist):
             profildict[name] = party
         player.append(name)
 
+
     for dmg in soup.find_all(attrs={"class": "yellow"}):
         if counter % 2 == 0:
             damage.append(dmg.get_text())
@@ -116,7 +116,6 @@ async def getPlayerDamage(url,session,profildict,partylist):
     for count, pl in enumerate(player):
         playerdamagedict[pl] = damage[count]
 
-    playerpartys = {}
     for name in playerdamagedict:
         if profildict[name] in partylist:
             if name in playerpartys:
@@ -125,7 +124,7 @@ async def getPlayerDamage(url,session,profildict,partylist):
                 playerpartys[name] = playerdamagedict[name]
 
     print("RawDamage URL: ", url)
-    print("In RawDamage." , playerpartys)
+    print("In RawDamage" , playerpartys)
 
     return playerpartys,profildict
 
